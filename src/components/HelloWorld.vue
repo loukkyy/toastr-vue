ac73c08b83375fda6424bba42139ade6823b338a
loukkyy <servan.fichet@gmail.com>
2021-07-09 18:57:17 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <button @click="toastMe">Click me</button>
+</template>
+
+<script>
+import NotificationDownload from "./NotificationDownload.vue"
+import { createToast } from "mosha-vue-toastify"
+import 'mosha-vue-toastify/dist/style.css'
+export default {
+  name: "HelloWorld",
+  components: {
+    NotificationDownload,
+  },
+  setup() {
+    const toastMe = () => {
+      console.log("it works")
+      createToast(NotificationDownload, {
+        transition: "slide",
+        position: "top-left",
+        type: "info",
+        showIcon: true,
+        swipeClose: true,
+        hideProgressBar: false,
+        timeout: -1,
+        onClose: () => {
+          console.log("log")
+        },
+      })
+    }
+    return { toastMe }
+  },
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/NotificationDownload.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Download</h1>
+  <a href="#">Download me</a>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
